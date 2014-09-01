10110df5b0747eb2ca676072d88be0833098d02d
Gillinghammer <colingillingham@gmail.com>
2014-09-01 13:07:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RockOnApp::Application.config.secret_token = '7b95247b2cf907fe0ec9d7b1b51e8cbed5a2b53ee9f3eef0446e16f0bdad5549ce596802e506d36be1057ba4048f493061ab835b1c44c24f8f8e5837ab9ea65b'
